083df168a9d41c1e025bd2156c69288f08383930
txm529 <73220726+txm529@users.noreply.github.com>
2023-02-13 23:10:45 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,5 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+<link rel="stylesheet" href="//g.alicdn.com/de/prismplayer/2.9.18/skins/default/aliplayer-min.css" /> <script type="text/javascript" src="//g.alicdn.com/de/prismplayer/2.9.18/aliplayer-min.js"></script> <script> var palyerDom = document.createElement('div'); palyerDom.id = 'J_prismPlayer'; palyerDom.style.width = '1920px'; palyerDom.style.height = '1280px'; palyerDom.className = 'prism-player'; var body = document.getElementsByTagName('body'); if(body.length) { body[0].appendChild(palyerDom); var player = new Aliplayer({ id: "J_prismPlayer", autoplay: true, width:"1920px", height:"1280px", vid:"73e76d10abaa71ed88786632b68f0102", playauth:'', cover:'' }); } </script>
